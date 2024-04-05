9e2b2485a4e450cdef23f28d3f7a12f49978bf18
s.ditaranto <simodita99@gmail.com>
2024-04-05 16:18:22 +0200

    feat: Hello World

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
+
+    <style>
+
+        .hello {
+            padding: 10px 0;
+
+            text-align: center;
+
+            font-size: 4em;
+
+            border: 1px solid black;
+            border-radius: 10px;
+            
+            color: green;
+        }
+
+    </style>
+
+
 </head>
 <body>
 
     <h1>Laravel</h1>
+
+    <div class="hello">
+        Hello World!
+    </div>
     
 </body>
 </html>
